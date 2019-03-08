51c046f0eec4da652f0fc0d2490231d330af3735
fran <francisco.pastor@colba.es>
2019-03-08 12:03:11 +0100

    init

---
+++ CameraQRNS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraQRNS
+//
+//  Created by Francisco Pastor Agusti on 08/03/2019.
+//  Copyright © 2019 Francisco Pastor Agusti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
