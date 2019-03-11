78c54ffcd513a36565fd508d55625ef13612e642
fran <francisco.pastor@colba.es>
2019-03-11 14:42:07 +0100

    Spinner rendered after 2 seconds of stop session

---
+++ CameraQRNS/SpinnerController.swift
@@ -0,0 +1,25 @@
+//
+//  SpinnerController.swift
+//  CameraQRNS
+//
+//  Created by Francisco Pastor Agusti on 11/03/2019.
+//  Copyright © 2019 Francisco Pastor Agusti. All rights reserved.
+//
+
+import UIKit
+
+class SpinnerViewController: UIViewController {
+    var spinner = UIActivityIndicatorView(style: .whiteLarge)
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.0)
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        view.addSubview(spinner)
+        
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
--- CameraQRNS/ViewController.swift
+++ CameraQRNS/ViewController.swift
@@ -1,31 +1,167 @@
 //
 //  ViewController.swift
-//  CameraQRNS
+//  Camera
 //
-//  Created by Francisco Pastor Agusti on 08/03/2019.
-//  Copyright © 2019 Francisco Pastor Agusti. All rights reserved.
+//  Created by Rizwan on 16/06/17.
+//  Copyright © 2017 Rizwan. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    var qrCodeFrameView: UIView?
+    var bgImage: UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video as the media type parameter
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+            fatalError("No video device found")
+        }
         
+        do {
+            
+            
+            // Get an instance of the AVCaptureDeviceInput class using the previous deivce object
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            // Initialize the captureSession object
+            captureSession = AVCaptureSession()
+            
+            // Set the input devcie on the capture session
+            captureSession?.addInput(input)
+            
+            // Get an instance of ACCapturePhotoOutput class
+            
+            // Set the output on the capture session
+            // Initialize a AVCaptureMetadataOutput object and set it as the input device
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            
+            //Initialise the video preview layer and add it as a sublayer to the viewPreview view's layer
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = previewView.layer.bounds
+            
+            
+             let image: UIImage = UIImage(named: "qrImage")!
+            
+         
+            bgImage = UIImageView(image: image)
+            bgImage?.alpha = CGFloat(0.3)
+            
+            bgImage?.frame = CGRect(
+                x:bgImage!.frame.origin.x,
+                y:bgImage!.frame.origin.y, width:200, height:200);
+            
+            previewView.layer.addSublayer(videoPreviewLayer!)
+            
+            previewView.addSubview(bgImage!)
+            //start video capture
+            captureSession?.startRunning()
+            
+            messageLabel.isHidden = true
+            
+            //Initialize QR Code Frame to highlight the QR code
+            qrCodeFrameView = UIView()
+
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                previewView.addSubview(qrCodeFrameView)
+                previewView.bringSubviewToFront(qrCodeFrameView)
+            }
+        } catch {
+            //If any error occurs, simply print it out
+            print(error)
+            return
+        }
         
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidLayoutSubviews() {
+        videoPreviewLayer?.frame = previewView.layer.bounds
+        if let previewLayer = videoPreviewLayer ,(previewLayer.connection?.isVideoOrientationSupported)! {
+            previewLayer.connection?.videoOrientation = UIApplication.shared.statusBarOrientation.videoOrientation ?? .portrait
+        }
     }
-    */
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
 
+extension ViewController : AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ captureOutput: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is contains at least one object.
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.isHidden = true
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.isHidden = false
+                messageLabel.text = metadataObj.stringValue
+                captureSession?.stopRunning()
+                 createSpinnerView()
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
+                    self.captureSession?.startRunning()
+                })
+            }
+        }
+    }
+    func createSpinnerView() {
+        let child = SpinnerViewController()
+        
+        // add the spinner view controller
+        addChild(child)
+        child.view.frame = previewView.bounds
+        previewView.addSubview(child.view)
+        child.didMove(toParent: self)
+        
+        // wait two seconds to simulate some work happening
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            // then remove the spinner view controller
+            child.willMove(toParent: nil)
+            child.view.removeFromSuperview()
+            child.removeFromParent()
+        }
+    }
+}
+
+extension UIInterfaceOrientation {
+    var videoOrientation: AVCaptureVideoOrientation? {
+        switch self {
+        case .portraitUpsideDown: return .portraitUpsideDown
+        case .landscapeRight: return .landscapeRight
+        case .landscapeLeft: return .landscapeLeft
+        case .portrait: return .portrait
+        default: return nil
+        }
+    }
 }
